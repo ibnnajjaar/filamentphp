1e54b89619c2e688ab7d4bc7a4b2abedf36c4ef9
Hussain Afeef <ibnahnajjaar@gmail.com>
2024-04-22 10:43:25 +0500

    feat: update npm packages

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,16 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            hotFile: 'public/app.hot',
+            buildDirectory: 'app',
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
+    build: {
+        manifest: 'manifest.json'
+    },
 });
